b175d3f986d86c4c428a552099d9a2b2c74f86bd
Jan <log@janwalenda.de>
2022-09-24 14:06:59 +0200

    Fixed for TypeScript

--- src/functions/others.ts
+++ src/functions/others.ts
@@ -1,28 +1,28 @@
-/**
- * @function URL.stringToURL()
- * @param {*} string 
- * @param {*} type 
- * @returns {string} url
- */
-const stringToURL = (string, type) => `data:${type};base64,${btoa(string)}`;
+// /**
+//  * @function URL.stringToURL()
+//  * @param {*} string 
+//  * @param {*} type 
+//  * @returns {string} url
+//  */
+// const stringToURL = (string: any, type: any): string => `data:${type};base64,${btoa(string)}`;
 
- /**
-  * Creates style from Object
-  * @param {{}} style 
-  * @returns {void} No Return
-  */
+//  /**
+//   * Creates style from Object
+//   * @param {{}} style 
+//   * @returns {void} No Return
+//   */
  
  
- function style(ctx: CanvasRenderingContext2D, style: { [key: string]: string }) {
-     if (style) {
-         for (const prop in style) {
-             if (prop in ctx) {
-                 if (typeof ctx[prop] === "function") {
-                     ctx[prop](style[prop]);
-                 } else {
-                     ctx[prop] = style[prop];
-                 }
-             }
-         }
-     }
- }
+//  function style(ctx: CanvasRenderingContext2D, style: { [key: string]: string }): void {
+//      if (style) {
+//          for (const prop in style) {
+//              if (prop in ctx) {
+//                  if (typeof ctx[prop] === "function") {
+//                      ctx[prop](style[prop]);
+//                  } else {
+//                      ctx[prop] = style[prop];
+//                  }
+//              }
+//          }
+//      }
+//  }
